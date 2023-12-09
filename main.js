6d1ecbc41d67f551d1526a0dff7fa4669d979575
Dale Fielding <dalefielding28@gmail.com>
2023-12-09 14:02:13 +0000

    Add settimeout function to display data after retrieved/stored, also delete setdata function.

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 let xhr = new XMLHttpRequest();
 let data;
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/"); // retrieves the URL, were making a requests to the ci-swapi api 
-xhr.send(); // sends the request 
+// retrieves the URL, were making a requests to the ci-swapi api 
+xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
 
-// Used to store the data that is returned from onreadystatechange.
-function setData(jsonData) {
-  data = jsonData;
-  console.log(data);
-}
-/*
-The xhr object maintains an internal state as it's completed various parts of our request operation
-Ready state of 4 means that the operation has been completed + status 200 means request succeeded and content delivered.
-Once completed we parse the responseText (string) into a json object and pass as a parameter to the setData function.
-*/
+// sends the request 
+xhr.send();
+
+// The xhr object maintains an internal state as it's completed various parts of our request operation.
+// Ready state of 4 means that the operation has been completed + status 200 means request succeeded and content delivered.
+// Once completed we parse the responseText (string) into a json object and pass as a parameter to the setData function.
 xhr.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
-    setData(JSON.parse(this.responseText));
+    data = JSON.parse(this.responseText);
   }
 };
+
+// Used to ensure the onreadystatechange function has completing it function and and storing the data, before logging data to the console.
+setTimeout(function () {
+  console.log(data);
+}, 500);
