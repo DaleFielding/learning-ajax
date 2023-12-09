f7efb539206d4c49f2bab7f95d6ba4332ca897d4
Dale Fielding <dalefielding28@gmail.com>
2023-12-09 14:36:25 +0000

    Add callback function and delete the settimeout function

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-let xhr = new XMLHttpRequest();
-let data;
+/*
+printDataToConsole(data) is passed as a parameter to getData. 
+Inside the function this is represented by name 'callback'
+As such data becomes callback(JSON.parse(this.responseText));
+*/
 
-// retrieves the URL, were making a requests to the ci-swapi api 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+function getData(callback) {
+  let xhr = new XMLHttpRequest();
 
-// sends the request 
-xhr.send();
+  xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+  xhr.send();
 
-// The xhr object maintains an internal state as it's completed various parts of our request operation.
-// Ready state of 4 means that the operation has been completed + status 200 means request succeeded and content delivered.
-// Once completed we parse the responseText (string) into a json object and pass as a parameter to the setData function.
-xhr.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    data = JSON.parse(this.responseText);
-  }
-};
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      callback(JSON.parse(this.responseText));
+    }
+  };
+}
 
-// Used to ensure the onreadystatechange function has completing it function and and storing the data, before logging data to the console.
-setTimeout(function () {
-  console.log(data);
-}, 500);
+function printDataToConsole(data) {
+  console.log(data)
+}
+
+getData(printDataToConsole);
