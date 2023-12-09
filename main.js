af6738df2b11cb8b75bc350e77ebe91b52186dbe
Dale Fielding <dalefielding28@gmail.com>
2023-12-09 13:50:06 +0000

    Add function (setdata) to store the parsed json data

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 let xhr = new XMLHttpRequest();
+let data;
 
 xhr.open("GET", "https://ci-swapi.herokuapp.com/api/"); // retrieves the URL, were making a requests to the ci-swapi api 
 xhr.send(); // sends the request 
 
+// Used to store the data that is returned from onreadystatechange.
+function setData(jsonData) {
+  data = jsonData;
+  console.log(data);
+}
 /*
 The xhr object maintains an internal state as it's completed various parts of our request operation
 Ready state of 4 means that the operation has been completed + status 200 means request succeeded and content delivered.
-Once completed we access the div with the id of data, then set its inner html to the response text that comes back from our xhr object
+Once completed we parse the responseText (string) into a json object and pass as a parameter to the setData function.
 */
 xhr.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
-    document.getElementById("data").innerHTML = this.responseText;
+    setData(JSON.parse(this.responseText));
   }
 };
